276dc79287f6bbe76e6fb9aa70aa73827dce3bbd
WILLOT Philippe <willot.ph@gmail.com>
2021-12-21 14:02:08 +0000

    fix pep8

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for letters in range(122, 96, -2):
-    print("{:s}{:s}".format(chr(letters), chr(letters - 33)), end= "")
-
+    print("{:s}{:s}".format(chr(letters), chr(letters - 33)), end="")
