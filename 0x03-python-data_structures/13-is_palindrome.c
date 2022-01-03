167e734380fbdd4d37e9abff292e10dd1add7384
Philippe WILLOT <willot.ph@gmail.com>
2022-01-03 07:09:55 -0800

    Trying with malloc dynamic memory allocation

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,22 +10,35 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *copy = *head;
-	int length = 0, i = 0, array[4096];
+	int length = 0, i = 0, *array;
 
 	if (*head)
 	{
 		while (copy)
 		{
-			array[length] = copy->n;
 			copy = copy->next;
 			length++;
 		}
+		array = malloc(sizeof(int) * length);
+		if (array == NULL)
+			return (0);
+		while (copy)
+		{
+			array[i] = copy->n;
+			copy = copy->next;
+			i++;
+		}
+		i = 0;
 		while (i < (length / 2))
 		{
 			if (array[i] != array[length - 1 - i])
+			{
+				free(array);
 				return (0);
+			}
 			i++;
 		}
 	}
+	free(array);
 	return (1);
 }
