de19064a512bacb72ead8dc5906bf56dffb9a0c1
WILLOT Philippe <willot.ph@gmail.com>
2022-01-17 11:33:17 +0000

    Try to fix requirement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     try:
         for elt in range(0, x):
-            print(my_list[elt], end="")
+            print("{:d}".format(my_list[elt]), end="")
         return x
     except IndexError:
         return elt
