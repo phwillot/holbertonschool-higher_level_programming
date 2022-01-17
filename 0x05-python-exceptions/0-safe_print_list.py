ae4762e3eb0347923fa622e0f1ecf7b79a05ee2a
WILLOT Philippe <willot.ph@gmail.com>
2022-01-17 11:29:14 +0000

    Set up folder and add task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for elt in range(0, x):
+            print(my_list[elt], end="")
+        return x
+    except IndexError:
+        return elt
+    finally:
+        print()
