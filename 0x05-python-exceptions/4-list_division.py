99f258a640dde5b344fb0a7f1704def577bcf2ac
WILLOT Philippe <willot.ph@gmail.com>
2022-01-17 12:42:54 +0000

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+            new_list.append(result)
+        except ZeroDivisionError:
+            new_list.append(0)
+            print("division by 0")
+        except IndexError:
+            new_list.append(0)
+            print("out of range")
+        except TypeError:
+            new_list.append(0)
+            print("wrong type")
+    return new_list
