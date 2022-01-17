33d2c8fcb0e57b26a4006eaa33bfdb5b6b8c8d7e
WILLOT Philippe <willot.ph@gmail.com>
2022-01-17 13:26:17 +0000

    Any exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,9 +5,9 @@ import sys
 def safe_function(fct, *args):
     try:
         fct(*args)
-        return fct(*args)
     except Exception as e:
         sys.stderr.write("Exception: ")
         sys.stderr.write(str(e))
         sys.stderr.write("\n")
         return None
+    return fct(*args)
