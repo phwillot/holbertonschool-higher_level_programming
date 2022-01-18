9b7e51a49572b7c54b7bb024088852847a02d81f
WILLOT Philippe <willot.ph@gmail.com>
2022-01-18 16:07:35 +0000

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square module.
+
+This modules contains a class who defines a square class
+
+"""
+
+
+class Square:
+    """ Defines a square. """
+    def __init__(self, size=0):
+        """Initializes the data"""
+
+        """ size: Size of the square"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
