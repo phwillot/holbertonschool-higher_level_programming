0039816a3fc8b838022092caa5db00edc22eb0b0
WILLOT Philippe <willot.ph@gmail.com>
2022-01-18 16:00:20 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module.
+
+This modules contains a class who defines a square class
+
+"""
+
+
+class Square:
+    """ Defines a square. """
+    def __init__(self, size):
+        """Initializes the data"""
+
+        """ size: Size of the square"""
+        self.__size = size
