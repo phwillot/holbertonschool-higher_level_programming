4039e3cb224495974efac2fee22256303b3bab47
WILLOT Philippe <willot.ph@gmail.com>
2022-01-26 13:21:55 +0000

    fix bug

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,16 +8,6 @@ def add_integer(a, b=98):
     """Returns the sum of a + b"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    else:
-        try:
-            a = int(a)
-        except Exception:
-            raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    else:
-        try:
-            b = int(b)
-        except Exception:
-            raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
