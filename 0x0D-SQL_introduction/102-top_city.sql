43c72aa55448fc97d2c02ae52a7e4d4feb0a115f
WILLOT Philippe <willot.ph@gmail.com>
2022-03-08 14:09:33 +0000

    fix task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,4 @@
 -- Displays the top 3 of cities temperature
 -- during July and August ordered by temperature
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = '8' OR month = '9'
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+
