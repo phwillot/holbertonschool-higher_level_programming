3c4cef5ff0788f52fe6fb37f37c5f481cce9beb5
WILLOT Philippe <willot.ph@gmail.com>
2022-03-08 11:53:39 +0000

    Adding CAPS to from

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Computes the score of all records
 -- in the table second_table of the database hbtn_0c_0
 -- Result column name is called average
-SELECT AVG(`score`) AS `average` from `second_table`;
+SELECT AVG(`score`) AS `average` FROM `second_table`;
