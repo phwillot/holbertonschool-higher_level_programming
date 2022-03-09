6a6c136f6c5b8ccf2cdf00522cf103692fdaa333
WILLOT Philippe <willot.ph@gmail.com>
2022-03-09 11:53:04 +0000

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- List all the cities of California that
 -- can be found in the database htbn_od_usa
-SELECT id, name FROM cities WHERE state_id = 1 ORDER BY name ASC
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC
