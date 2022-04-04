043cd3ee664bfd43ccd5aef57d707018d88e177c
WILLOT Philippe <willot.ph@gmail.com>
2022-04-04 11:23:11 +0000

    fixing negative number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
   const intArray = process.argv.slice(2).map(Number);
-  intArray.sort();
+
+  intArray.sort(function (a, b) { return a - b; });
   intArray.pop();
   console.log(intArray.slice(-1)[0]);
 } else console.log(0);
