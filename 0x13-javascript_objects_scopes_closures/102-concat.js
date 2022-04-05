cbab2c9d0f1a30e88d38837e9f74dca6a00aa682
WILLOT Philippe <willot.ph@gmail.com>
2022-04-05 11:38:59 +0000

    Add last task advanced of the project

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const file1 = fs.readFileSync(process.argv[2], 'utf-8');
+const file2 = fs.readFileSync(process.argv[3], 'utf-8');
+
+fs.appendFileSync(process.argv[4], file1 + file2);
