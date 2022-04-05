2ca21e6eaed4a2c4c6eeb11f3d6b1e7aab3ffa45
WILLOT Philippe <willot.ph@gmail.com>
2022-04-05 10:46:57 +0000

    method fix task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const reversed = [];
-  for (let i = list.length - 1; i >= 0; i--) reversed.unshift(list[i]);
+  for (let i = list.length - 1; i >= 0; i--) reversed.push(list[i]);
   return reversed;
 };
