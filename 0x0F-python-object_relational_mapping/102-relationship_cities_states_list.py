fb81b92c9b8c5f8032d32baab9b7e072d05f8a0a
WILLOT Philippe <willot.ph@gmail.com>
2022-04-08 13:17:41 +0000

    add last advanced task

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa"""
-from relationship_state import Base, State
+"""Lists all City objects from the database hbtn_0e_101_usa"""
+from relationship_state import State
 from relationship_city import City
-from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 import sys
 
 
@@ -20,6 +19,5 @@ if __name__ == "__main__":
     states = session.query(State).all()
 
     for state in states:
-        print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
+            print(f"{city.id}: {city.name} -> {state.name}")
