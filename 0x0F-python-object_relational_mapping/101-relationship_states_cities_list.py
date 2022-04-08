86988275e14bef54309fce63429cc2e4975e16f0
WILLOT Philippe <willot.ph@gmail.com>
2022-04-08 12:43:40 +0000

    add task 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""creates the State “California” with the City “San Francisco”
-from the database hbtn_0e_100_usa"""
+"""lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy.orm import Session
@@ -15,10 +15,12 @@ if __name__ == "__main__":
             sys.argv[2],
             sys.argv[3]),
         pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     session = Session(engine)
-    newState = State(name="California")
-    newState.cities.append(City(name="San Francisco"))
-    session.add(newState)
-    session.commit()
-    session.close()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        cities = session.query(City).order_by(
+            City.id).filter(state.id == City.state_id).all()
+        for city in cities:
+            print(f"\t{city.id}: {city.name}")
