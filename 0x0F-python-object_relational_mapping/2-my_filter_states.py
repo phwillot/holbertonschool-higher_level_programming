a11a3941c17ab24a6999a02acd532b6337b99997
WILLOT Philippe <willot.ph@gmail.com>
2022-04-09 06:02:18 +0000

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
         db=sys.argv[3],
         charset="utf8")
     cur = db.cursor()
-    cur.execute(
-        "SELECT id, name FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(
-            sys.argv[4]))
+    cur.execute("SELECT id, name FROM states WHERE name LIKE BINARY\
+                '{}' ORDER BY id".format(
+        sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
