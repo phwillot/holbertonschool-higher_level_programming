1463566a09be2a81f95ae0a3a1eeed8895d6aefe
Philippe Willot <willot.ph@gmail.com>
2022-04-21 21:47:39 +0200

    Modifying task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,18 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers"""
-    if len(list_of_integers) == 0:
+    length = len(list_of_integers)
+    if length == 0:
         return None
-    return max(list_of_integers)
+    elif length == 1:
+        return list_of_integers[0]
+    else:
+        for i in range(0, length - 1):
+            peak = list_of_integers[i]
+            if i == 0 and length <= 1:
+                if peak > list_of_integers[i + 1]:
+                    return peak
+            else:
+                if peak >= list_of_integers[i - 1] and\
+                        peak >= list_of_integers[i + 1]:
+                    return peak
