51c232976a80410cae97dbfb1b172a85615b7460
Philippe Willot <willot.ph@gmail.com>
2022-04-22 08:42:36 +0200

    Fixing requirement task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print(f"\t- type: {type(html)}")
