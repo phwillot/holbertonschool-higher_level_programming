6dfb00d54f6712b4e60fc3840fd9e8508cfa0049
Philippe Willot <willot.ph@gmail.com>
2022-05-17 13:14:09 +0200

    Fix semistandard on task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  console.log(data)
-})
+  console.log(data);
+});
